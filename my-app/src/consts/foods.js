8709caf25ae10584cd3712a0bbdc08185e55395a
bimakrishna <krishna.bima@yahoo.com>
2021-01-23 18:27:59 +0700

    inspirasi

---
+++ my-app/src/consts/foods.js
@@ -0,0 +1,32 @@
+const foods = [
+  {
+    id: '1',
+    name: 'Ibu Litha',
+    ingredients: 'Mathematics',
+    price: '100.000',
+    image: require('../assets/Ibu.Litha.png'),
+  },
+  {
+    id: '2',
+    name: 'Bpk. Sugeng',
+    ingredients: 'B.Indonesia',
+    price: '120.000',
+    image: require('../assets/Bpk.Sugeng.png'),
+  },
+  {
+    id: '3',
+    name: 'Bpk. Farhat',
+    ingredients: 'Pakar Ekonomi',
+    price: '130.000',
+    image: require('../assets/Bpk.Farhat.png'),
+  },
+  {
+    id: '4',
+    name: 'Bpk. Zaidan',
+    ingredients: 'pakar Biologi',
+    price: '140.000',
+    image: require('../assets/Bpk.Zaidan.png'),
+  },
+];
+
+export default foods;
